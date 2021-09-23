1820dd9055f4fbcadb1ba84749a2842a98b22963
admin <admin@YD03-16>
2021-09-23 17:26:56 +0900

    210923 닥터 대쉬보드 및 예약관리 화면구현

--- Mima/src/main/java/com/mima/app/doc/controller/PatnerDoctorController.java
+++ Mima/src/main/java/com/mima/app/doc/controller/PatnerDoctorController.java
@@ -1,8 +1,21 @@
 package com.mima.app.doc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/docDash/*")
 public class PatnerDoctorController {
+	
+	@GetMapping("docMain")
+	public void docMain() {}
+	
+	@GetMapping("apptManage")
+	public void apptManage() {}
 
+	@GetMapping("apptManageCal")
+	public void apptManageCal() {}
+
+	
 }
