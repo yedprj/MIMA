920414c9f35df60cb061b57b98c3101641b7a941
Sasha <Sasha@DESKTOP-VQLDAG4>
2021-09-27 18:48:52 +0900

    210927 닥터 대쉬보드 메인 나의후기 요약 구현

--- Mima/src/main/java/com/mima/app/comments/service/CommentsService.java
+++ Mima/src/main/java/com/mima/app/comments/service/CommentsService.java
@@ -1,10 +1,13 @@
 package com.mima.app.comments.service;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Param;
 
 import com.mima.app.comments.domain.CommentsPageVO;
 import com.mima.app.comments.domain.CommentsVO;
 import com.mima.app.criteria.domain.Criteria;
+import com.mima.app.session.domain.BookingVO;
 
 public interface CommentsService {
 	//CURD
@@ -16,4 +19,6 @@ public interface CommentsService {
 		//게시글번호에 해당하는 댓글 조회
 		public CommentsPageVO getList(@Param("cri") Criteria cri,  @Param("cmainCategory") String cmainCategory, @Param("cmainNo") int cmainNo);
 
+		// 닥터 대쉬보드 나의 후기
+		public List<CommentsVO> getlatestreviewList();
 }
