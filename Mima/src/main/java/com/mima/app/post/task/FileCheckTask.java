6d990119a66614e4e466678a7d264584a5c0f2da
Cbear1234 <amywonderland77@gmail.com>
2021-09-29 20:22:31 +0900

    scheduler -juyun

--- Mima/src/main/java/com/mima/app/post/task/FileCheckTask.java
+++ Mima/src/main/java/com/mima/app/post/task/FileCheckTask.java
@@ -1,6 +1,9 @@
 package com.mima.app.post.task;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -18,7 +21,7 @@ public class FileCheckTask {
 	@Autowired PostService postService;
 	@Autowired BookingService bookingService;
 	
-	@Scheduled(cron="0 0 0 * * *")
+	@Scheduled(cron="5 0 0 * * *")
 	public void checkFiles() throws Exception{
 		
 		log.info("지난 날짜 삭제 확인");
@@ -28,10 +31,17 @@ public class FileCheckTask {
 		log.info("==================삭제확인");
 	}
 
-
-	@Scheduled(cron="*/10 0 8-18 * * MON-FRI")
+	//s:0929 
+	@Scheduled(cron="*/10 0 8-22 * * MON-FRI")
 	public void checkRoomId() {
-		log.info("진료를 위한 url을 확인중입니다...");
-		bookingService.getRoomId();
+		log.info("~~~~~진료를 위한 url을 확인중입니다...~~~~~~~~");
+		List<BookingVO> oldList = new ArrayList<BookingVO>();
+		List<BookingVO> readyList = new ArrayList<BookingVO>();
+		readyList = bookingService.getRoomId();
+		oldList = readyList;
+		if(oldList.size() < readyList.size()) {
+			
+		}
+		
 	}
 }
