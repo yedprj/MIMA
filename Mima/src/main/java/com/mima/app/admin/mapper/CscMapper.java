0f57b5725adeea3240860363c7e6828e21bb02de
Sasha <Sasha@DESKTOP-VQLDAG4>
2021-09-29 19:51:14 +0900

    210929 닥터 대쉬보드 예약관리 페이지 기능 구현. 진료내역/나의 문의/나의 후기/나의 환자들 페이지 화면 구현.

--- Mima/src/main/java/com/mima/app/admin/mapper/CscMapper.java
+++ Mima/src/main/java/com/mima/app/admin/mapper/CscMapper.java
@@ -1,5 +1,11 @@
 package com.mima.app.admin.mapper;
 
+import java.util.List;
+
+import com.mima.app.admin.domain.CscVO;
+
 public interface CscMapper {
 
+	// 닥터 대쉬보드 나의 문의 페이지_J29
+	public List<CscVO> docQna();
 }
--- Mima/src/main/java/com/mima/app/session/service/BookingService.java
+++ Mima/src/main/java/com/mima/app/session/service/BookingService.java
@@ -6,8 +6,16 @@ import com.mima.app.session.domain.BookingVO;
 
 public interface BookingService {
 
+	// 닥터 대쉬보드 메인 페이지 오늘의 예약_J
 	public List<BookingVO> getList();
+		
+	// 닥터 대쉬보드 메인 페이지 진료내역_J
 	public List<BookingVO> getlatestapptList();
+	
+	// 닥터 대쉬보드 예약관리 페이지_J
 	public List<BookingVO> apptList();
+	
+	// 닥터 대쉬보드 진료내역 페이지_J29
+	public List<BookingVO> apptHistoryList();
 
 }
