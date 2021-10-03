253f2f8bb3c11786363feba4886173ecdd31d397
Coby <dngur1278@gmail.com>
2021-10-03 21:55:31 +0900

    .. 로그아웃 안되는 부분 있어서 수정 .. 쿠키 해야됨 ..

--- Mima/src/main/java/com/mima/app/member/controller/LoginController.java
+++ Mima/src/main/java/com/mima/app/member/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.mima.app.member.controller;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,7 +12,7 @@ public class LoginController {
 	
 	// 로그인 폼으로 이동
 	@GetMapping("/login")
-	public String loginForm(String error, String logout, Model model) {
+	public String loginForm(String error, String logout, Model model, HttpServletResponse response) {
 		
 		if (error != null) {
 			model.addAttribute("error", "아이디 또는 비밀번호가 틀렸거나 아직 승인되지 않았습니다.");
@@ -21,9 +22,10 @@ public class LoginController {
 			model.addAttribute("logout", "로그아웃 하셨습니다.");
 		}
 		
-		Cookie cookie = new Cookie("role", null);
-		cookie.setComment("Role을 가지는 쿠키");
-		cookie.setMaxAge(60*60*24*365);
+		/*
+		 * Cookie cookie = new Cookie("role", null); cookie.setComment("Role을 가지는 쿠키");
+		 * cookie.setMaxAge(60*60*24*365); response.addCookie(cookie);
+		 */
 		
 		return "member/loginForm";
 	}
--- Mima/src/main/java/com/mima/app/session/controller/BookingController.java
+++ Mima/src/main/java/com/mima/app/session/controller/BookingController.java
@@ -1,15 +1,29 @@
 package com.mima.app.session.controller;
 
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.mima.app.member.domain.MemberVO;
+import lombok.extern.java.Log;
+
+@Log
 @Controller
 public class BookingController {
 
 	// 결제 예약 페이지로 이동 p.01
 	@GetMapping("/reservationForm")
-	public String reservationForm() {
+	public String reservationForm(HttpServletRequest request) {
 		
+		/*
+		 * HttpSession session = request.getSession();
+		 * log.info(session.getAttribute("session").toString()); 
+		 * MemberVO mvo = (MemberVO) session.getAttribute("session"); 
+		 * log.info(mvo.getName());
+		 */
 		return "booking/reservationForm";
 	}
 }
